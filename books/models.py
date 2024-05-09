8bac448e487b39e53cd0fb44fb31cde8ca36cc9b
MagdiWaleed <mmmaaagggdddiii@gmail.com>
2024-05-09 10:32:37 +0300

    done

---
+++ books/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from profileModel.models import ProfileModel
+# Create your models here.
+
+class Book(models.Model):
+    title=models.CharField(max_length=50)
+    description= models.CharField(max_length=50)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    img= models.ImageField(upload_to='books/%y/%m/%d',default='default-book-cover.jpg')
+    author_name= models.CharField(max_length=50)
+    about_author= models.TextField()
+    category=models.CharField(max_length=50,null=True,default="no category")
+    book_type=models.CharField(default="latest",max_length=50,  choices=[("trending","trending"),("latest","latest")],null=True)
+    user= models.ForeignKey(ProfileModel,models.SET_NULL,null=True)
+    def __str__(self):
+        return str(self.title)
+
---
+++ filtering/static/filtering/js/borrowed_books_controller.js
@@ -0,0 +1,14 @@
+function goToSingleAuthor(author_name){
+        window.location.href=`http://127.0.0.1:8000/filtering/authors/${author_name}`
+    }
+
+function goToSingleUser(id){
+    window.location.href=`http://127.0.0.1:8000/filtering/users/${id}`
+}
+
+
+    
+function goToSingleBook(id){
+        window.location.href= `http://127.0.0.1:8000/books/${id}`
+  }
+  
