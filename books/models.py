2a23fa20ceba6ff53598cf2b347833bf2a583247
MagdiWaleed <mmmaaagggdddiii@gmail.com>
2024-05-22 02:52:26 +0300

    adding mine feature and trending feature

--- books/models.py
+++ books/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from profileModel.models import ProfileModel
+from django.utils import timezone
 # Create your models here.
 
 class Book(models.Model):
@@ -13,6 +14,13 @@ class Book(models.Model):
     category=models.CharField(max_length=50,null=True,default="no category")
     is_trending=models.BooleanField(default=False)
     user= models.ForeignKey(ProfileModel,models.SET_NULL,blank=True,null=True)
+    trending_date= models.DateTimeField(null=True,blank=True)
+
+    def mark_as_trending(self):
+        self.is_trending = True
+        self.trending_date = timezone.now()
+        self.save()
+
     def __str__(self):
         return str(self.title)
 
